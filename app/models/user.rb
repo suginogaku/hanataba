3da40edece40ea3e4b70d1ba420c08aa11e1bd4d
suginogaku <gakusugino@icloud.com>
2020-11-29 10:00:45 +0900

    deviseビューファイルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # emailとpasswordは元々presence: trueがついてるから新たにつける必要はないけど、rails側にも一応つけた
   with_options presence: true do
-   validates :name
-   validates :email
-   validates :password
+    validates :name
+    validates :email
+    validates :password
   end
   validates :email, uniqueness: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください' }
