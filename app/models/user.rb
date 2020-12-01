1a2d9c7699b35a487d6dacfb0f9aa13314203873
suginogaku <gakusugino@icloud.com>
2020-12-01 17:13:48 +0900

    エラーメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates :name, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # emailとpasswordは元々presence: trueがついてるから新たにつける必要はないけど、rails側にも一応つけた
-  with_options presence: true do
-    validates :name
-    validates :email
-    validates :password
-  end
+
   validates :email, uniqueness: true
   validates :password, length: { minimum: 6 }
 end
