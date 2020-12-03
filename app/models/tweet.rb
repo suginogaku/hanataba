473b6c06eacbeb3df3091f04f9fc886d6084d7a0
suginogaku <gakusugino@icloud.com>
2020-12-03 18:23:51 +0900

    activehash導入

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
