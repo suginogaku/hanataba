04fac65d4a2981d48a83fcac0a339f2c6b940f64
suginogaku <gakusugino@icloud.com>
2020-12-05 10:22:08 +0900

    投稿ページ実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,11 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+  
 end
