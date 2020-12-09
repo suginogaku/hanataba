88e5f221fcad5c317c57332312b94ffa7d08cce0
suginogaku <gakusugino@icloud.com>
2020-12-09 12:54:45 +0900

    新しい順に投稿内容を表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   # before_action :authenticate_user!, except: [:index, :show]
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order('created_at DESC')
   end
 
   def new
