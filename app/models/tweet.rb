20ff0444f0c1068b39373feccaffa6021cdce564
suginogaku <gakusugino@icloud.com>
2020-12-17 11:19:05 +0900

    検索処理の記述

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,4 +7,13 @@ class Tweet < ApplicationRecord
 
   validates :image, :title, presence: true
   # validates :prefecture_id, numericality: { other_than: 1 } 
+
+  # 検索処理の記述
+  def self.search(search)
+    if search != "" #検索フォームに何か値が入力されているかどうかで処理を分岐
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
